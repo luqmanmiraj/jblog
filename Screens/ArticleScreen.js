ab51a568ebf8153d03a5487f517f778b871bb59e
Maniha-Affaf <affaf.maniha@gmail.com>
2024-06-09 09:14:23 +0500

    changes

--- Screens/ArticleScreen.js
+++ Screens/ArticleScreen.js
@@ -1,20 +1,118 @@
 
 
+
+// import React, { useState, useEffect, useContext  } from 'react';
+// import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+// import { useNavigation } from '@react-navigation/native';
+//  import { CategoryContext, SubCategoryContext } from '../components/Context/BreadcrumbContext';
+//  import AsyncStorage from '@react-native-async-storage/async-storage';
+
+// export default function ArticleScreen({ route }) {
+//   const { category, subcategory } = route.params;
+//   const [articles, setArticles] = useState([]);
+//   const [selectedArticle, setSelectedArticle] = useState(null);
+
+//   const { setCategory } = useContext(CategoryContext);
+//   const { setSubcategory } = useContext(SubCategoryContext);
+
+//   const navigation = useNavigation();
+
+//   const handleArticlePress = (article) => {
+//     setSelectedArticle(article)
+//     navigation.navigate('HTMLScreen', { article: article.title, id: article.id, type: article.type });
+//   }
+
+
+//   useEffect(() => {
+//     setCategory(category);
+//     setSubcategory(subcategory);
+
+//     const loadCachedData = async () => {
+//       try {
+//         const cachedDataString = await AsyncStorage.getItem(`articles_${category}_${subcategory}`);
+//         if (cachedDataString) {
+//           const cachedData = JSON.parse(cachedDataString);
+//           setArticles(cachedData.data);
+     
+//         }
+//         fetchArticles(); // Fetch fresh data in the background
+//       } catch (error) {
+//         console.error('Error loading cached data:', error);
+//       }
+//     };
+
+//     loadCachedData();
+//     fetchArticles();
+    
+//   }, [category, subcategory]);
+
+
+
+
+
+
+
+
+//        const fetchArticles = async () => {
+//           try {
+//             const response = await fetch(
+//               `https://4uvh5c5t2g.execute-api.us-east-1.amazonaws.com/dev/items?tech=${category}&parent=${subcategory}`,
+//             );
+//             if (!response.ok) {
+//               throw new Error(`HTTP error! status: ${response.status}`);
+//             }
+//             const jsonData = await response.json();
+//             setArticles(jsonData);
+      
+//             const dataToSave = {
+//               data: jsonData,
+//               timestamp: Date.now(),
+//             };
+      
+//             try {
+//               await AsyncStorage.setItem(`articles_${category}_${subcategory}`, JSON.stringify(dataToSave));
+//               console.log('Data saved to AsyncStorage');
+//             } catch (error) {
+//               console.error('Error saving data to AsyncStorage:', error);
+//             }
+//           } catch (error) {
+//             console.error('Error fetching articles:', error);
+//           }
+//         };
+//   return (
+//     <View style={styles.main}>
+//       <Text style={styles.TextHeading}>Articles</Text>
+//       <Text style={styles.header}>{category} / {subcategory}</Text>
+//       <ScrollView contentContainerStyle={styles.scrollView}>
+//         {articles.map((article, index) => (
+//           <TouchableOpacity key={index} onPress={() => handleArticlePress(article)}>
+//             <View style={styles.item}>
+//               <Text style={styles.content}>{article.title}</Text>
+//             </View>
+//           </TouchableOpacity>
+//         ))}
+//       </ScrollView>
+//     </View>
+//   );
+// }
+
+
 import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation } from '@react-navigation/native';
 import { CategoryContext, SubCategoryContext } from '../components/Context/BreadcrumbContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function ArticleScreen({ route }) {
+  const { category, subcategory } = route.params;
+  const [articles, setArticles] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const { setCategory } = useContext(CategoryContext);
   const { setSubcategory } = useContext(SubCategoryContext);
 
-  const { category, subcategory } = route.params;
-  const [articles, setArticles] = useState([]);
   const navigation = useNavigation();
-  
+
   const handleArticlePress = (article) => {
     navigation.navigate('HTMLScreen', { article: article.title, id: article.id, type: article.type });
   };
@@ -28,10 +126,11 @@ export default function ArticleScreen({ route }) {
         const cachedDataString = await AsyncStorage.getItem(`articles_${category}_${subcategory}`);
         if (cachedDataString) {
           const cachedData = JSON.parse(cachedDataString);
-          setArticles(cachedData.data);
-     
+          if (Array.isArray(cachedData.data)) {
+            setArticles(cachedData.data);
+          }
         }
-        fetchArticles(); 
+        fetchArticles(); // Fetch fresh data in the background
       } catch (error) {
         console.error('Error loading cached data:', error);
       }
@@ -49,7 +148,11 @@ export default function ArticleScreen({ route }) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
       const jsonData = await response.json();
-      setArticles(jsonData);
+      if (Array.isArray(jsonData)) {
+        setArticles(jsonData);
+      } else {
+        console.error('Fetched data is not an array:', jsonData);
+      }
 
       const dataToSave = {
         data: jsonData,
@@ -64,12 +167,25 @@ export default function ArticleScreen({ route }) {
       }
     } catch (error) {
       console.error('Error fetching articles:', error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <Text style={styles.loadingText}>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (!Array.isArray(articles) || articles.length === 0) {
+    return <Text style={styles.content}>No articles available</Text>;
+  }
+
   return (
     <View style={styles.main}>
-  
       <Text style={styles.TextHeading}>Articles</Text>
       <ScrollView contentContainerStyle={styles.scrollView}>
         {articles.map((article, index) => (
@@ -85,11 +201,13 @@ export default function ArticleScreen({ route }) {
 }
 
 
+
+
 const styles = StyleSheet.create({
   main: {
     padding: 20,
     flex: 1,
-    backgroundColor: '#F0F0F0', 
+    backgroundColor: '#F0F0F0', // White background
   },
   scrollView: {
     flexGrow: 1,
@@ -97,15 +215,15 @@ const styles = StyleSheet.create({
   item: {
     padding: 10,
     marginVertical: 5,
-    borderRadius: 10, 
+    borderRadius: 10, // Unified borderRadius
     backgroundColor: '#FFFFFF',
-    borderColor: '#CCCCCC', 
+    borderColor: '#CCCCCC', // Light border color
     borderWidth: 1,
   },
   content: {
     fontSize: 16,
-    color: 'black',
-    fontFamily: 'Helvetica', 
+    color: 'black', // Dark text color
+    fontFamily: 'Helvetica', // Helvetica font family
   },
   header: {
     color: "#007AFF",
@@ -123,3 +241,136 @@ const styles = StyleSheet.create({
     fontFamily: 'Helvetica',
   },
 });
+
+
+
+
+
+
+// import React, { useState, useEffect, useContext } from 'react';
+// import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { useNavigation } from '@react-navigation/native';
+// import { CategoryContext, SubCategoryContext } from '../components/Context/BreadcrumbContext';
+
+// export default function ArticleScreen({ route }) {
+
+//   const { setCategory } = useContext(CategoryContext);
+//   const { setSubcategory } = useContext(SubCategoryContext);
+
+// const [selectedArticle, setSelectedArticle] = useState(null);
+//   const { category, subcategory } = route.params;
+//   const [articles, setArticles] = useState([]);
+//   const navigation = useNavigation();
+  
+//   const handleArticlePress = (article) => {
+//     setSelectedArticle(article)
+//     navigation.navigate('HTMLScreen', { article: article.title, id: article.id, type: article.type });
+//   };
+
+//   useEffect(() => {
+//     setCategory(category);
+//     setSubcategory(subcategory);
+
+//     const loadCachedData = async () => {
+//       try {
+//         const cachedDataString = await AsyncStorage.getItem(`articles_${category}_${subcategory}`);
+//         if (cachedDataString) {
+//           const cachedData = JSON.parse(cachedDataString);
+//           setArticles(cachedData.data);
+     
+//         }
+//         fetchArticles(); // Fetch fresh data in the background
+//       } catch (error) {
+//         console.error('Error loading cached data:', error);
+//       }
+//     };
+
+//     loadCachedData();
+    
+//   }, [category, subcategory]);
+
+//   const fetchArticles = async () => {
+//     try {
+//       const response = await fetch(
+//         `https://4uvh5c5t2g.execute-api.us-east-1.amazonaws.com/dev/items?tech=${category}&parent=${subcategory}`,
+//       );
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       const jsonData = await response.json();
+//       setArticles(jsonData);
+
+//       const dataToSave = {
+//         data: jsonData,
+//         timestamp: Date.now(),
+//       };
+
+//       try {
+//         await AsyncStorage.setItem(`articles_${category}_${subcategory}`, JSON.stringify(dataToSave));
+//         console.log('Data saved to AsyncStorage');
+//       } catch (error) {
+//         console.error('Error saving data to AsyncStorage:', error);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching articles:', error);
+//     }
+//   };
+//   console.log("articles:",articles);
+//   return (
+//     <View style={styles.main}>
+  
+//       <Text style={styles.TextHeading}>Articles</Text>
+//       <ScrollView contentContainerStyle={styles.scrollView}>
+  
+//         {articles.map((article, index) => (
+//           <TouchableOpacity key={index} onPress={() => handleArticlePress(article)}>
+//             <View style={styles.item}>
+//               <Text style={styles.content}>{article.title}</Text>
+//             </View>
+//           </TouchableOpacity>
+//         ))}
+//       </ScrollView>
+//     </View>
+//   );
+// }
+
+
+// const styles = StyleSheet.create({
+//   main: {
+//     padding: 20,
+//     flex: 1,
+//     backgroundColor: '#F0F0F0', // White background
+//   },
+//   scrollView: {
+//     flexGrow: 1,
+//   },
+//   item: {
+//     padding: 10,
+//     marginVertical: 5,
+//     borderRadius: 10, // Unified borderRadius
+//     backgroundColor: '#FFFFFF',
+//     borderColor: '#CCCCCC', // Light border color
+//     borderWidth: 1,
+//   },
+//   content: {
+//     fontSize: 16,
+//     color: 'black', // Dark text color
+//     fontFamily: 'Helvetica', // Helvetica font family
+//   },
+//   header: {
+//     color: "#007AFF",
+//     paddingVertical: 5,
+//     fontWeight: "bold",
+//     fontSize: 22,
+//     fontFamily: 'Helvetica',
+//   },
+//   TextHeading: {
+//     color: "#333333",
+//     fontSize: 28,
+//     textAlign: 'center',
+//     margin: 10,
+//     fontWeight: 'bold',
+//     fontFamily: 'Helvetica',
+//   },
+// });
--- Screens/SubCategories.js
+++ Screens/SubCategories.js
@@ -1,5 +1,142 @@
 
 
+// import React, { useState, useEffect } from 'react';
+// import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { useNavigation } from '@react-navigation/native';
+
+// export default function SubCategories({ route }) {
+//   const { category } = route.params;
+//   const [subcategories, setSubcategories] = useState([]);
+//   const [selectedSubCategory, setSelectedSubCategory] = useState(null);
+//   const navigation = useNavigation();
+
+//   useEffect(() => {
+//     const loadCachedData = async () => {
+//       try {
+//         const cachedDataString = await AsyncStorage.getItem(`subcategories_${category}`);
+//         if (cachedDataString) {
+//           const cachedData = JSON.parse(cachedDataString);
+//           const isFresh = isDataFresh(cachedData.timestamp);
+
+//           if (isFresh) {
+//             setSubcategories(cachedData.data);
+//           } else {
+//             console.log('Cached data is not fresh, fetching new data');
+//             fetchSubcategories();
+//           }
+//         } else {
+//           fetchSubcategories();
+//         }
+//       } catch (error) {
+//         console.error('Error loading cached data:', error);
+//       }
+//     };
+
+//     loadCachedData();
+//   }, [category]);
+
+//   const fetchSubcategories = async () => {
+//     try {
+//       const response = await fetch(`https://khedu5yl4a.execute-api.us-east-1.amazonaws.com/dev/items?tech=${category}`);
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! status: ${response.status}`);
+//       }
+//       const jsonData = await response.json();
+//       setSubcategories(jsonData);
+
+//       const dataToSave = {
+//         data: jsonData,
+//         timestamp: Date.now(),
+//       };
+
+//       try {
+//         await AsyncStorage.setItem(`subcategories_${category}`, JSON.stringify(dataToSave));
+//         console.log('Data saved to AsyncStorage');
+//       } catch (error) {
+//         console.error('Error saving data to AsyncStorage:', error);
+//       }
+//     } catch (error) {
+//       console.error('Error fetching subcategories:', error);
+//     }
+//   };
+
+//   const isDataFresh = (cachedDataTimestamp) => {
+//     const freshnessThresholdInMs = 1000 * 60 * 60; // Example: 1 hour
+//     const currentTime = Date.now();
+//     return (currentTime - cachedDataTimestamp) < freshnessThresholdInMs;
+//   };
+
+//   const handleSubCategoryPress = (subcategory) => {
+//     setSelectedSubCategory(subcategory);
+//     navigation.navigate('ArticleScreen', { category: category, subcategory: subcategory });
+//   };
+
+//   if (!Array.isArray(subcategories) || subcategories.length === 0) {
+//     return <Text style={styles.content}>No subcategories available</Text>;
+//   }
+
+//   return (
+//     <View style={styles.main}>
+//       <Text style={styles.TextHeading}>Chapters</Text>
+//       <Text style={styles.header}>{category}</Text>
+//       <ScrollView contentContainerStyle={styles.scrollView}>
+//         {subcategories.map((subcategory, index) => (
+//           <TouchableOpacity key={index} onPress={() => handleSubCategoryPress(subcategory)}>
+//             <View style={styles.item}>
+//               <Text style={styles.content}>{subcategory}</Text>
+//             </View>
+//           </TouchableOpacity>
+//         ))}
+//       </ScrollView>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   main: {
+//     padding: 20,
+//     flex: 1,
+//     backgroundColor: '#153448',
+//   },
+//   scrollView: {
+//     flexGrow: 1,
+//   },
+//   item: {
+//     padding: 10,
+//     marginVertical: 5,
+//     borderRadius: 5,
+//     backgroundColor: '#FFFFFF1A',
+//     width: '100%',
+//     borderColor:"#FFFFFF99",
+//     borderWidth:1,
+//     borderRadius:10
+//   },
+//   content: {
+//     fontSize: 16,
+//     color: 'white',
+//   },
+//   header: {
+//     color: "#66E4FE",
+//     paddingVertical: 5,
+//     fontWeight: "bold",
+//     fontSize:20,
+    
+//   },
+//   TextHeading: {
+//     color:"white",
+//     fontSize:26,
+//     textAlign: 'center',
+//     margin:10,
+//     fontWeight: 'bold',
+//       },
+// });
+
+
+
+
+
+
 
 import React, { useState, useEffect, useContext } from 'react';
 import { View, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
@@ -12,6 +149,7 @@ export default function SubCategories({ route }) {
   const { setSubcategory } = useContext(SubCategoryContext);
 
   const { category } = route.params;
+  const [selectedSubCategory, setSelectedSubCategory] = useState(null);
   const [subcategories, setSubcategories] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const navigation = useNavigation();
@@ -26,7 +164,7 @@ export default function SubCategories({ route }) {
           const cachedData = JSON.parse(cachedDataString);
           setSubcategories(cachedData.data);
         }
-        fetchSubcategories(); 
+        fetchSubcategories(); // Fetch fresh data in the background
       } catch (error) {
         console.error('Error loading cached data:', error);
       }
@@ -62,8 +200,10 @@ export default function SubCategories({ route }) {
     }
   };
 
+
   const handleSubCategoryPress = (subcategory) => {
-    navigation.navigate('AppNavigation', { category: category, subcategory: subcategory });
+    setSelectedSubCategory(subcategory); 
+     console.log("subcategory", subcategory);
     navigation.navigate('ArticleScreen', { category: category, subcategory: subcategory });
   };
 
